02c7c5411f36bccbb0c323f6e7c84e20540ab033
MilenaMaced <milenasantosmcd@gmail.com>
2019-05-14 14:13:35 -0300

    Conforme a Issue #1
    Adicionada a classe de configurações do banco de dados

--- src/br/com/ufrpeuag/gastromaster/negocio/modelo/classes/Mesa.java
+++ src/br/com/ufrpeuag/gastromaster/negocio/modelo/classes/Mesa.java
@@ -3,7 +3,13 @@ package br.com.ufrpeuag.gastromaster.negocio.modelo.classes;
 public class Mesa {
 	
 	private int id_mesa;
-	
-	
+
+	public int getId_mesa() {
+		return id_mesa;
+	}
+
+	public void setId_mesa(int id_mesa) {
+		this.id_mesa = id_mesa;
+	}
 
 }
